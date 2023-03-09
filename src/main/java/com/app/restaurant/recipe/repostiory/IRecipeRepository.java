eb21774947d4851fee2e2369f3dfcf375441ea94
anitham2009 <anitham2009@gmail.com>
2023-03-10 02:39:10 +0530

    Updated comment

--- src/main/java/com/app/restaurant/recipe/repostiory/IRecipeRepository.java
+++ src/main/java/com/app/restaurant/recipe/repostiory/IRecipeRepository.java
@@ -22,7 +22,7 @@ public interface IRecipeRepository extends JpaRepository<Recipe, Long> {
 	@Query(value="select distinct ri.* from recipe ri inner join recipe_ingredient rii on ri.recipe_id=rii.recipe_id  inner join ingredient ii on rii.ingredient_id=ii.ingredient_id inner join recipe_instruction rins on ri.recipe_id=rins.recipe_id  where  (:isVeg is null or lower(ri.is_veg) = :isVeg)  and (:includeIngredient is null or lower(ii.ingredient_name) like :includeIngredient%) and ri.recipe_id not in ( Select  ring.recipe_id from recipe_ingredient ring where ring.ingredient_id in ( select  ing.ingredient_id  from ingredient ing where lower(ing.ingredient_name)  like :excludeIngredient% )) and  (:noOfServings is null or ri.no_of_servings >= :noOfServings) and (:instructions is null or (lower(rins.cooking_method) like %:instructions% or lower(rins.cooking_steps) like %:instructions%  or lower(rins.additional_info) like %:instructions%))",nativeQuery = true)
 	List<Recipe>  findByRecipeDetail(@Param("isVeg") String isVeg, @Param("noOfServings") Integer noOfServings, @Param("includeIngredient") String includeIngredient, @Param("excludeIngredient") String excludeIngredient,
 			@Param("instructions") String instruction);
-	//Get recipe detail o given recipe name 
+	//Get recipe detail of given recipe name (case insensitive search)
 	Recipe findByRecipeNameIgnoreCase(String recipeName);
 
 }
